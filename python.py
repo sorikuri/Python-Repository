8c86dc6e662e2bfccfdbc3c8162a85a03f4de55f
sorikuri <sobicodo@gmail.com>
2025-12-27 15:50:44 +0500

    Initial commit

---
+++ python.py
@@ -0,0 +1,13 @@
+str1 = "Yo! My bro whatsuppp~"
+len1= len (str1)
+print (len1)
+str2 = "I'm Sobia bro."
+len2 = len (str2)
+print (len2)
+str_name = str1+" "+str2
+print (str_name)
+print(len(str_name))
+
+print(str1[7])
+str ="GoodMorning"
+print (str[-8:-1])
