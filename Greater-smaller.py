a85a8bef011c88a79cf697a07809bca1869ef923
sorikuri <sobicodo@gmail.com>
2025-12-29 14:05:12 +0500

    Condition to find largest number amoung three given numbers.

---
+++ Greater-smaller.py
@@ -0,0 +1,9 @@
+num1 = int(input("Enter num1 = "))
+num2 = int(input("Enter num2 = "))
+num3 = int(input("Enter num3 = "))
+if (num1 > num2 and num1 > num3):
+    print ("Num1 is greatest amoung all.",num1)
+elif(num2 > num1 and num2 > num3):
+    print ("Num2 is greatest amoung all.",num2)
+else:
+    print ("Num3 is greatest amoung all.",num3)
---
+++ even-odd.py
@@ -0,0 +1,5 @@
+num = float (input("Enter any number = "))
+if (num % 2 == 0):
+    print ("It's an Even Number.")
+else:
+    print ("It's an Odd Number.")
